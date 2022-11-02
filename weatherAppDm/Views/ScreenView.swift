463ac393d7a4452008b1906308ac0c46169384cd
davidmansourian <mada20sv@student.ju.se>
2022-11-02 15:40:57 +0100

    Implemented design example of like button for liking weather page and saving. Will work well when models and view models have been implemented

--- weatherAppDm/Views/ScreenView.swift
+++ weatherAppDm/Views/ScreenView.swift
@@ -8,19 +8,20 @@
 import SwiftUI
 
 struct ScreenView: View {
-    @State public var showSearchBar = false
+    @State private var showSearchBar = false
+    @State private var isLiked = false
     var body: some View {
         NavigationStack{
             ZStack{
-//                LinearGradient(gradient: Gradient(colors:[.orange,.blue]), startPoint: UnitPoint(x:1.9, y: -0.05), endPoint: UnitPoint(x: 0.4, y: 1))
-//                    .edgesIgnoringSafeArea(.all)
+                //                LinearGradient(gradient: Gradient(colors:[.orange,.blue]), startPoint: UnitPoint(x:1.9, y: -0.05), endPoint: UnitPoint(x: 0.4, y: 1))
+                //                    .edgesIgnoringSafeArea(.all)
                 LinearGradient(gradient: Gradient(colors:[.yellow,.blue,.blue]), startPoint: .topTrailing, endPoint: .bottomLeading) // Change color depending on time of day and weather
                     .edgesIgnoringSafeArea(.all)
                 ScrollView(.vertical){
-                        VStack(alignment: .center, spacing: 15) {
-                            TodaysWeatherTextView()
-                            TodaysWeatherView()
-                            ForecastView()
+                    VStack(alignment: .center, spacing: 15) {
+                        TodaysWeatherTextView()
+                        TodaysWeatherView()
+                        ForecastView()
                     }
                 }
                 .onTapGesture {
@@ -51,12 +52,25 @@ struct ScreenView: View {
                     }
                     if !showSearchBar{
                         ToolbarItemGroup(placement: .navigationBarLeading){
-                            NavigationLink(destination:{}){
-                                Image(systemName: "heart")
-                                    .foregroundColor(Color.black)
-                                    .padding(10)
-                                    .background(Color.white.opacity(0.4))
-                                    .clipShape(Circle())
+                            Button {
+                                withAnimation(.easeInOut(duration: 0.1)){
+                                    isLiked.toggle()
+                                }
+                            } label: {
+                                if isLiked{
+                                    Image(systemName: "heart.fill")
+                                        .foregroundColor(Color.red)
+                                        .padding(10)
+                                        .background(Color.white.opacity(0.4))
+                                        .clipShape(Circle())
+                                }
+                                else{
+                                    Image(systemName: "heart")
+                                        .foregroundColor(Color.black)
+                                        .padding(10)
+                                        .background(Color.white.opacity(0.4))
+                                        .clipShape(Circle())
+                                }
                             }
                         }
                     }

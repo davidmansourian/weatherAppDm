5b5708f2e7208836b4f6a76b17e5f3196880de7c
davidmansourian <mada20sv@student.ju.se>
2022-11-04 01:38:15 +0100

    hourly forecast now showing api information, however, rough implementation, not ideal design pattern

--- weatherAppDm/Views/TodaysWeatherView.swift
+++ weatherAppDm/Views/TodaysWeatherView.swift
@@ -7,6 +7,12 @@
 
 import SwiftUI
 
+extension StringProtocol {
+    subscript(offset: Int) -> Character {
+        self[index(startIndex, offsetBy: offset)]
+    }
+}
+
 struct TodaysWeatherView: View {
     
     func getTemperatureForI() -> ([Int]){
@@ -20,11 +26,18 @@ struct TodaysWeatherView: View {
         return temperatures
     }
     
+    func getTimeForI() -> ([String]){
+        var times: [String] = []
+        for time in weatherModel.hourlyWeather?.time ?? [""]{
+            times.append(time)
+        }
+        return times
+    }
+    
     @StateObject private var weatherModel = APILoader()
-    var times = ["Now", "7 am", "8 am", "9 am", "10 am", "11 am", "12 am", "13 am", "14 am", "15 am"]
-    var grads = ["10"]
     var body: some View {
-        var temperatures: [Int] = getTemperatureForI()
+        let temperatures: [Int] = getTemperatureForI()
+        let times: [String] = getTimeForI()
         
         VStack(spacing: 10){
             ZStack{
@@ -36,24 +49,28 @@ struct TodaysWeatherView: View {
                     .frame(width: 365)
                     .offset(y: -35)
                 HStack {
-                    Text("Wheather info text from ViewModel. Bla Bla Bla fasdf")
+                    Text("Wheather info text from ViewModel. Bla Bla Bla fasdf") // create a function that
+                    // calculates an average on the weathercode. Make a scoring system that mentions the
+                    // most frequent code in words first, and then the second. E.g. Mostly overcast, some sun.
                         .offset(y: -70)
                         .foregroundColor(Color.white)
                         .font(Font.footnote)
                 }
                 ScrollView(.horizontal, showsIndicators: false) {
                     VStack(spacing: 15){
-                        HStack(spacing: 30) {
-//                            ForEach(times, id: \.self) { time in
-//                                Text(time)
-//                                    .frame(width: 46, height: 30)
-//                                    .bold()
-//                                    .font(.system(size: 13))
-//                                    .foregroundColor(Color.white)
-//                            }
+                        HStack(spacing: 26.3) {
+                            ForEach(times.prefix(20), id: \.self) { time in
+                                let desiredStringOne = time.suffix(1)
+                                let desiredStringTwo = time.dropFirst(11).dropLast(3)
+                                Text(desiredStringTwo)
+                                    .frame(width: 46, height: 30)
+                                    .bold()
+                                    .font(.system(size: 13))
+                                    .foregroundColor(Color.white)
+                            }
                             
-//                            ForEach(theDate, id: \.self){ time in
-//                                Text((time?[12] ?? "") + (time?[13] ?? ""))
+//                            ForEach(times.prefix(20), id: \.self){ time in
+//                                Text(time[12] + time[13])
 //                                    .frame(width: 46, height:30)
 //                                    .bold()
 //                                    .font(.system(size: 13))
